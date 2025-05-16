9143da5dfb8c87df8e4e73f9fcd2207888b7c15c
bricenodev <miguelbricenocode@gmail.com>
2025-05-16 17:58:35 +0200

    feat(services): API services for main entities

---
+++ src/services/authAPI.ts
@@ -0,0 +1,118 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { CheckPasswordForm, ConfirmToken, ForgotPasswordForm, NewPasswordForm, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm, UserSchema } from "../types";
+
+export async function createAccount(formData: UserRegistrationForm){
+  try {
+
+    const url = `/auth/create-account`
+    const { data } = await api.post<string>(url, formData)
+    return data 
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function confirmAccount(token: ConfirmToken){
+  try {
+    const url = `/auth/confirm-account`
+    const { data } = await api.post<string>(url, token)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function requestConfirmationCode(formData: RequestConfirmationCodeForm){
+  try {
+    const url = `/auth/request-code`
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function login(formData: UserLoginForm){
+  try {
+    const url = `/auth/login`
+    const { data } = await api.post<string>(url, formData)
+    console.log(data)
+    localStorage.setItem('AUTH_TOKEN', data)
+    console.log(data)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function forgotPassword(formData: ForgotPasswordForm){
+  try {
+    const url = `/auth/forgot-password`
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function validateToken(formData: ConfirmToken){
+  try {
+    const url = `/auth/validate-password`
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function updatePasswordWithToken({formData, token}:{formData: NewPasswordForm, token: ConfirmToken['token']}){
+  try {
+    const url = `/auth/update-password/${token}`
+    const { data } = await api.put<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function getUser(){
+  try {
+    const { data } = await api('/auth/user')
+    const response = UserSchema.safeParse(data)
+    if(response.success){
+      return response.data
+    }
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function checkPassword( formData : CheckPasswordForm){
+  try {
+    const url = '/auth/check-password'
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
---
+++ src/services/projectAPI.ts
@@ -0,0 +1,103 @@
+import { DashboardProjectSchema, EditProjectSchema, Project, ProjectFormData, ProjectSchema } from "@/types/index";
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+
+
+
+export async function createProject(formData:ProjectFormData){
+  console.log(formData)
+  try {
+    const {data} = await api.post('/projects', formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+
+export async function getProjects(){
+  try {
+    const { data } = await api('/projects')
+    const response = DashboardProjectSchema.safeParse(data)
+    if(response.success){
+      return response.data
+    }
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function getProjectById(id : Project['_id']){
+  try {
+    const { data } = await api(`/projects/${id}`);
+    const response = EditProjectSchema.safeParse(data);
+    if(response.success){
+      return response.data;
+    } else {
+      throw new Error("Error de validación de esquema Zod");
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+    throw error;
+  }
+}
+
+
+export async function getFullProject(id : Project['_id']){
+  try {
+    console.log(id)
+    const { data } = await api(`/projects/${id}`)
+    console.log(data)
+    const response = ProjectSchema.safeParse(data)
+    console.log(response)
+    if (response.success) {
+      return response.data
+    } else {
+      throw new Error("La validación del esquema falló")
+    }
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+    // por si es otro error
+    throw error
+  }
+}
+
+type ProjectAPIType = {
+  formData: ProjectFormData,
+  projectId: Project['_id']
+}
+
+export async function updateProject({formData, projectId }:ProjectAPIType){
+  try {
+
+    const {data} = await api.put<string>(`/projects/${projectId}`, formData)
+    return data
+
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function deleteProject(id : Project['_id']){
+  try {
+    const { data } = await api.delete(`/projects/${id}`)
+    return data
+   
+  } catch (error) {
+    if(isAxiosError(error) && error.response){
+      throw new Error(error.response.data.error)
+    }
+    
+  }
+}
+
